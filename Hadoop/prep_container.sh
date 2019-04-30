51f1d25d06535890ee510e1a103c16362ea7d641
Stephen Jusuf <29591527+steven-jay@users.noreply.github.com>
2019-04-30 21:36:49 +1000

    Updated script to run more scripts

--- Hadoop/prep_container.sh
+++ Hadoop/prep_container.sh
@@ -22,8 +22,11 @@ fi
 echo "Copying DataStax Repo to container\n"
 docker cp "${HADOOP_PATH}datastax.repo" "${CONTAINER}:/etc/yum.repos.d/datastax.repo"
 docker cp "${HADOOP_PATH}setup_cassandra_table.cql" "${CONTAINER}:${HOME_PATH}setup_cassandra_table.cql"
+docker cp "${HADOOP_PATH}setup_mysql.sql" "${CONTAINER}:${HOME_PATH}setup_mysql.sql"
+docker cp "${HADOOP_PATH}create_movielens_table.sql" "${CONTAINER}:${HOME_PATH}create_movielens_table.sql"
 
-SCRIPTS=("download_files.sh" "install_cassandra.sh" "install_mongodb.sh")
+#SCRIPTS=("download_files.sh" "install_mysql.sh" "install_cassandra.sh" "install_mongodb.sh" "install_drill.sh")
+SCRIPTS=("install_drill.sh")
 for i in "${SCRIPTS[@]}"; do
   echo "Running ${i}"
   echo ""
